4473a5c69521cd58aba3041b633c723116d13707
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2006-10-29 11:49:08 +0000

    wrong xml2-include
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@40 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 g++ src/*.cpp src/client/*.cpp src/common/*.cpp src/server/*.cpp \
-	-I include -I libxml2 \
+	-I include -I /usr/include/libxml2 \
 	-lSDL -lSDL_image -lNL -lz -lgd -lxml2 \
 	-o bin/openlierox
