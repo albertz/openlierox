abd09f605cfc87beee26bbaf5484217a86b547ed
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2006-11-06 15:11:53 +0000

    fixed compile.sh
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@69 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- compile.sh
+++ compile.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-g++ src/*.cpp src/client/*.cpp src/common/*.cpp src/server/*.cpp \
+if [ ! -d bin ]; then mkdir bin; fi
+
+echo ">>> compiling now, this could take a little time ..."
+if g++ src/*.cpp src/client/*.cpp src/common/*.cpp src/server/*.cpp \
 	-I include -I /usr/include/libxml2 \
 	-lSDL -lSDL_image -lSDL_mixer -lNL -lz -lgd -lxml2 \
 	-o bin/openlierox
+then
+	echo ">>> success"
+	exit 0
+else
+	echo ">>> error(s) reported, check the output above"
+	exit -1
+fi
