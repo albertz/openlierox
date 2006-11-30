2bb91223435b571a5842d63c1b638c2ad66f1aa2
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2006-11-30 16:37:32 +0000

    new installer files
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@106 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- distrib/create_archives.sh
+++ distrib/create_archives.sh
@@ -9,10 +9,27 @@ STD_FILES="CMakeLists.txt compile.sh install.sh start.sh"
 DOC_FILES="COPYING.LIB DEPS doc/*"
 DAT_FILES="share/gamedir/* share/OpenLieroX.png"
 
-SRC_RELEASE="$SRC_FILES $STD_FILES $DOC_FILES $DAT_FILES"
-SRC_PREFIX="distrib/tarball/OpenLieroX_${VERSION}.src"
+export SRC_RELEASE="$SRC_FILES $STD_FILES $DOC_FILES $DAT_FILES"
+export SRC_PREFIX="distrib/tarball/OpenLieroX_${VERSION}.src"
 
 # TODO: BIN_RELEASE
 
-tar -jcf ${SRC_PREFIX}.tar.bz $SRC_RELEASE
-zip -r -9 ${SRC_PREFIX}.zip $SRC_RELEASE
+[ -e ${SRC_PREFIX}.tar.bz ] && rm ${SRC_PREFIX}.tar.bz
+[ -e ${SRC_PREFIX}.zip ] && rm ${SRC_PREFIX}.zip
+
+# this is a very very dirty hack, but I don't know how to do better
+export FILELIST=""
+for f in $SRC_RELEASE; do
+	# filter out the .svn shit
+	TMP=$(find "$f" -type f -printf "\"%h/%f\"\n" | grep -v .svn)
+	# remove the newlines -> fill FILELIST
+	for f2 in $TMP; do
+		FILELIST="$FILELIST $f2"
+	done
+done
+
+echo ">>> creating zip ..."
+sh -c "zip -r -9 ${SRC_PREFIX}.zip $FILELIST"
+
+echo ">>> creating tar.bz ..."
+tar --exclude=.svn -jcf ${SRC_PREFIX}.tar.bz $SRC_RELEASE
