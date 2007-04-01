afc319884e2c3e99b9cc3499be6ad7a7376f2a69
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2007-04-01 18:42:21 +0000

    removed double skins/default.png
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@475 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- distrib/web/index.php
+++ distrib/web/index.php
@@ -50,6 +50,9 @@
 	<a href="tarball/OpenLieroX_<?php echo $VERSION; ?>.src.tar.bz">OpenLieroX Source tar.bz</a><br>
 	<a href="tarball/OpenLieroX_<?php echo $VERSION; ?>.src.zip">OpenLieroX Source zip</a><br>
 	<a href="tarball/OpenLieroX_<?php echo $VERSION; ?>.win32.zip">OpenLieroX Win32 binary zip</a><br>
+	(HINT: These are snapshots I made from time to time.
+	Don't link directly to this files because I always will replace it with
+	new snapshots I will made.)
 	<br>
 	<b>other Download-sources</b><br>
 	Take also a look in the 
