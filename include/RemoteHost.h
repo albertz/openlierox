0edc70b88519338d217c6263f87281319eb18a17
non_apparent <non_apparent@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2007-07-30 13:22:11 +0000

    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@843 34602234-ff1f-0410-a465-ea8f3b77ab7f

---
+++ include/RemoteHost.h
@@ -0,0 +1,13 @@
+#ifndef __REMOTEHOST_H__
+#define __REMOTEHOST_H__
+
+#define HOST_ALL		0xffffffff
+#define HOST_AUTHORISE	0x1
+#define HOST_KICK		0x2
+#define HOST_MUTE		0x4
+#define HOST_BAN		0x8
+#define HOST_SETNAME	0x10
+#define HOST_SETCOLOUR	0x20
+#define HOST_SETSKIN	0x40
+
+#endif
