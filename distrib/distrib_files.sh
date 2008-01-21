7092814517412272575329f51315eb1d1620d50d
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-01-21 16:00:06 +0000

    removed unneeded old distrib files
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@1418 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- distrib/distrib_files.sh
+++ distrib/distrib_files.sh
@@ -4,16 +4,10 @@ DEST="albertzeyer@shell.sourceforge.net:/home/groups/o/op/openlierox/htdocs/"
 SYNC_CMD="rsync -avP --exclude .svn"
 
 # first, upload all new files
-$SYNC_CMD additions $DEST
-# $SYNC_CMD tarball $DEST
 $SYNC_CMD ebuild $DEST
-# $SYNC_CMD deb $DEST
 
 # now, update the site itself
 $SYNC_CMD web/* $DEST
 
 # and lastly, clean up old files
-$SYNC_CMD --delete-after additions $DEST
-# $SYNC_CMD --delete-after tarball $DEST
 $SYNC_CMD --delete-after ebuild $DEST
-# $SYNC_CMD --delete-after deb $DEST
--- distrib/receive_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SRC="albertzeyer@shell.sourceforge.net:/home/groups/o/op/openlierox/htdocs/"
-SYNC_CMD="rsync -avP --exclude .svn"
-
-# download all missing files
-$SYNC_CMD $SRC/additions .
-$SYNC_CMD $SRC/tarball .
-$SYNC_CMD $SRC/deb .
