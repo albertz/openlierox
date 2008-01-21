b47eb8af7f967efaa5baa914349d1b5d10f9c9d4
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-01-21 18:10:53 +0000

    small update in distrib-script
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@1422 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- distrib/distrib_files.sh
+++ distrib/distrib_files.sh
@@ -4,6 +4,7 @@ DEST="albertzeyer@shell.sourceforge.net:/home/groups/o/op/openlierox/htdocs/"
 SYNC_CMD="rsync -avP --exclude .svn"
 
 # first, upload all new files
+$SYNC_CMD tmpbuild $DEST
 $SYNC_CMD ebuild $DEST
 
 # now, update the site itself
@@ -11,3 +12,4 @@ $SYNC_CMD web/* $DEST
 
 # and lastly, clean up old files
 $SYNC_CMD --delete-after ebuild $DEST
+#$SYNC_CMD --delete-after tmpbuild $DEST
