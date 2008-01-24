136891eeb84fde1aecba58944fd150fcc6cd74e3
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-01-24 23:28:22 +0000

    faster distrib-script
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@1460 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- distrib/distrib_files.sh
+++ distrib/distrib_files.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-DEST="albertzeyer@shell.sourceforge.net:/home/groups/o/op/openlierox/htdocs/"
+SERVER="albertzeyer@shell.sourceforge.net"
+REMOTEDIR="/home/groups/o/op/openlierox/htdocs/"
+DEST="$SERVER:$REMOTEDIR"
 SYNC_CMD="rsync -avP --exclude .svn"
+
+[ -e tmpbuild/* ] 2>/dev/null && {
+	f=$(echo tmpbuild/*)
+	ssh $SERVER "cd $REMOTEDIR; [ -e tmpbuild/* ] 2>/dev/null && \
+		[ tmpbuild/* != $f ] && \
+		echo 'single tmpbuild-file, renaming' && \
+		mv tmpbuild/* $f"
+}
 
 # first, upload all new files
 $SYNC_CMD tmpbuild $DEST
