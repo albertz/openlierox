9a7edf6b17d10341efaef4e0ab9acb70bee84fb4
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-02-09 13:58:15 +0000

    missing file for abstracted physic engine
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@1595 34602234-ff1f-0410-a465-ea8f3b77ab7f

---
+++ src/common/Physics.cpp
@@ -0,0 +1,30 @@
+/*
+	OpenLieroX
+	
+	physic simulation interface
+	
+	code under LGPL
+	created on 9/2/2008
+*/
+
+#include <iostream>
+#include "Physics.h"
+#include "PhysicsLX56.h"
+
+using namespace std;
+
+static PhysicsEngine* engine;
+PhysicsEngine* PhysicsEngine::Get() { return engine; }
+void PhysicsEngine::Set(PhysicsEngine* e) { engine = e; }
+
+void PhysicsEngine::Init() {
+	engine = CreatePhysicsEngineLX56();
+	
+	cout << "PhysicsEngine " << engine->name() << " loaded" << endl;
+}
+
+void PhysicsEngine::UnInit() {
+	cout << "unloading PhysicsEngine " << engine->name() << " .." << endl;
+	delete engine;
+	engine = NULL;
+}
