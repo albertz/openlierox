0e11e2dda1a6905ba47ac79ad4eee02dd68205dd
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-02-09 16:16:56 +0000

    comment (goal for physics)
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@1601 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- include/Physics.h
+++ include/Physics.h
@@ -41,6 +41,7 @@ public:
 	virtual void uninitGame() = 0; // gives just a hint to the engine that the game isn't runnign anymore
 	
 	// TODO: later, we should have a class World and all objects and the map are included there
+	// in the end, I want to have one single simulate(CWorld* world);
 	virtual void simulateWorm(CWorm* worm, CWorm *worms, int local) = 0;
 	virtual void simulateWormWeapon(float dt, CWorm* worm) = 0;
 	virtual void simulateProjectile(CProjectile* proj, CWorm *worms, int *wormid, int* result) = 0;
