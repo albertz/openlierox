19644ba79cf9a181b06ef0308b788b2762842b6f
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-02-24 20:53:53 +0000

    removed debian source example files
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@1808 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- distrib/deb/create_deb_package.sh
+++ distrib/deb/create_deb_package.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cd ../.. # go into root dir
-dpkg-buildpackage -rfakeroot
+dpkg-buildpackage -rfakeroot \
+	-I.svn -Idistrib -ICMakeFiles -Ibuild -I*stamp -IMakefile \
+	-ICMakeCache.txt -Icmake* -Itools
