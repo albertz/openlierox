89b09e44868b721d2d570b7b3b0b0aaf1dd4fc47
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-03-09 08:14:48 +0000

    debian updates
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@1921 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- distrib/deb/create_deb_package.sh
+++ distrib/deb/create_deb_package.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 cd ../.. # go into root dir
-dpkg-buildpackage -rfakeroot \
+dpkg-buildpackage -rfakeroot -sa -sK -S \
 	-I.svn -Idistrib -ICMakeFiles -Ibuild -I*stamp -IMakefile \
-	-ICMakeCache.txt -Icmake* -Itools
+	-ICMakeCache.txt -Icmake* -Itools \
+	-i\
+"(?:^|/).*~$|"\
+"(?:^|/)\.#.*$|"\
+"(?:^|/)\..*\.swp$|"\
+"(?:^|/),,.*(?:$|/.*$)|"\
+"(?:^|/)(?:DEADJOE|\.cvsignore|\.arch-inventory|\.bzrignore)$|"\
+"(?:^|/)(?:CVS|RCS|\.deps|\{arch\}|\.arch-ids|\.svn|_darcs|\.git|\.bzr(?:\.backup|tags)?)(?:$|/.*$)$|"\
+"(?:^|/)(?:CMakeFiles|build|distrib|tools|Makefile|CMakeCache.txt|cmake)|"\
+"(?:^|/)bin/.*$"
