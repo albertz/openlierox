08a44bf8e704ff89fb2bd79c749aa60ed1dd1439
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-03-16 13:28:19 +0000

    GetAbsolutePath fix (you have to use GetExactFileName for non-win there), else realpath doesn't work)
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@1972 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- src/common/FindFile.cpp
+++ src/common/FindFile.cpp
@@ -759,10 +759,14 @@ std::string GetAbsolutePath(const std::string& path) {
 	else  // Failed
 		return path;
 #else
-	static char buf[MAXPATHLEN];
-	if(realpath(path.c_str(), buf) != NULL) {
-		fix_markend(buf);
-		return buf;
+	std::string exactpath;
+	if(GetExactFileName(path, exactpath)) {
+		static char buf[MAXPATHLEN];
+		if(realpath(exactpath.c_str(), buf) != NULL) {
+			fix_markend(buf);
+			return buf;
+		} else
+			return exactpath;
 	} else
 		return path;
 #endif
