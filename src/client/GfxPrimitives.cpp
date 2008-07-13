6262530b8280b7b00620d1b1f443e431b7a6a939
dark_charlie <dark_charlie@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-07-13 13:16:07 +0000

    Recompiled SDL with VC++ 2005 to run on older Windows, small fix in GfxPrimitives.cpp
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@2224 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- src/client/GfxPrimitives.cpp
+++ src/client/GfxPrimitives.cpp
@@ -916,7 +916,8 @@ static Uint32 HalfBlendPixel(Uint32 p1, Uint32 p2, Uint32 p3, Uint32 p4, SDL_Pix
 	return (Uint32) (
 		((((PIXEL_CHANNEL(p1, R) + PIXEL_CHANNEL(p2, R) + PIXEL_CHANNEL(p3, R) + PIXEL_CHANNEL(p4, R))) / 4) & fmt->Rmask) |
 		((((PIXEL_CHANNEL(p1, G) + PIXEL_CHANNEL(p2, G) + PIXEL_CHANNEL(p3, G) + PIXEL_CHANNEL(p4, G))) / 4) & fmt->Gmask) |
-		((((PIXEL_CHANNEL(p1, B) + PIXEL_CHANNEL(p2, B) + PIXEL_CHANNEL(p3, B) + PIXEL_CHANNEL(p4, B))) / 4) & fmt->Bmask) );
+		((((PIXEL_CHANNEL(p1, B) + PIXEL_CHANNEL(p2, B) + PIXEL_CHANNEL(p3, B) + PIXEL_CHANNEL(p4, B))) / 4) & fmt->Bmask) ) |
+		fmt->Amask; // HINT: this doesn't support semi-transparent surfaces
 }
 
 void DrawImageScaleHalf(SDL_Surface* bmpDest, SDL_Surface* bmpSrc) {
