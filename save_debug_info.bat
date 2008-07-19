ae03ae0b18b197c018ea83dc19c283d9c02ed433
dark_charlie <dark_charlie@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-07-19 17:14:26 +0000

    Enabled more optimizations and more debug info in the VC++ projects (for getting a better back trace from user feedback)
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@2243 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- save_debug_info.bat
+++ save_debug_info.bat
@@ -8,6 +8,10 @@ mkdir debuginfo\%1\sources\src\common
 mkdir debuginfo\%1\sources\src\server
 copy distrib\win32\OpenLieroX.exe debuginfo\%1\OpenLieroX.exe
 copy distrib\win32\OpenLieroX.pdb debuginfo\%1\OpenLieroX.pdb
+copy distrib\win32\OpenLieroX.map debuginfo\%1\OpenLieroX.map
+copy distrib\win32\SDL.pdb debuginfo\%1\SDL.pdb
+copy distrib\win32\SDL.dll debuginfo\%1\SDL.dll
+copy distrib\win32\vc*.?db debuginfo\%1\
 copy include\*.h debuginfo\%1\sources\include
 copy src\*.cpp debuginfo\%1\sources\src
 copy src\client\*.cpp debuginfo\%1\sources\src\client
