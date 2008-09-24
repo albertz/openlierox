9be7f8dec5e83f98b6be7c907bf76ca5153b3c66
dark_charlie <dark_charlie@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-09-24 08:16:33 +0000

    Added file for Options editable in game (first hackontest commit)
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@2374 34602234-ff1f-0410-a465-ea8f3b77ab7f

---
+++ src/client/DeprecatedGUI/Menu_FloatingOptions.cpp
@@ -0,0 +1,14 @@
+/////////////////////////////////////////
+//
+//             OpenLieroX
+//
+// code under LGPL, based on JasonBs work,
+// enhanced by Dark Charlie and Albert Zeyer
+//
+//
+/////////////////////////////////////////
+
+
+// Floating options window file
+// Created 24/9/08
+// Karel Petranek
