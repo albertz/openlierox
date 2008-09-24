145966c79131dbdcdb395566213bea7bebb43f52
pelya <pelya@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-09-24 08:52:32 +0000

    Hackontest: Update input keys in game, for Floating Options
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@2375 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- src/client/DeprecatedGUI/Menu_FloatingOptions.cpp
+++ src/client/DeprecatedGUI/Menu_FloatingOptions.cpp
@@ -12,3 +12,19 @@
 // Floating options window file
 // Created 24/9/08
 // Karel Petranek
+
+#include "Options.h"
+#include "CClient.h"
+
+void UpdateGameInputs()
+{
+	// Setup the controls
+	cClient->getWorm(0)->SetupInputs( tLXOptions->sPlayerControls[0] );
+	// TODO: setup also more viewports
+	if(cClient->getNumWorms() >= 2)
+		cClient->getWorm(1)->SetupInputs( tLXOptions->sPlayerControls[1] );
+	
+    cClient->getViewports()[0].setupInputs( tLXOptions->sPlayerControls[0] );
+    cClient->getViewports()[1].setupInputs( tLXOptions->sPlayerControls[1] );
+
+}
