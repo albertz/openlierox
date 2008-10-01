01357233f2c1f2d9a26e3a421a2c60fba8601942
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-10-01 15:57:14 +0000

    disable mouse aiming when app has no focus
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@2511 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- src/common/CWorm_Simulate.cpp
+++ src/common/CWorm_Simulate.cpp
@@ -57,7 +57,10 @@ void CWorm::getInput()
 	ws->bShoot = false;
 	ws->bJump = false;
 
-	const bool mouseControl = tLXOptions->bMouseAiming && ( cClient->isHostAllowingMouse() || tGameInfo.iGameType == GME_LOCAL);
+	const bool mouseControl = 
+			tLXOptions->bMouseAiming &&
+			( cClient->isHostAllowingMouse() || tGameInfo.iGameType == GME_LOCAL) &&
+			ApplicationHasFocus(); // if app has no focus, don't use mouseaiming, the mousewarping is pretty annoying then
 	const float mouseSensity = (float)tLXOptions->iMouseSensity; // how sensitive is the mouse in X/Y-dir
 
 	// TODO: here are width/height of the window hardcoded
