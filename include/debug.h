c571b9354c7c4973aa13f48e7523988ac8bd80bf
dark_charlie <dark_charlie@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-10-07 12:46:02 +0000

    Added missing debug.h file
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@2642 34602234-ff1f-0410-a465-ea8f3b77ab7f

---
+++ include/debug.h
@@ -0,0 +1,34 @@
+/////////////////////////////////////////
+//
+//             OpenLieroX
+//
+// code under LGPL, based on JasonBs work,
+// enhanced by Dark Charlie and Albert Zeyer
+//
+//
+/////////////////////////////////////////
+
+
+// debug header file
+// Contains tools for detecting memleaks and similar debug stuff
+
+
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+#ifdef DEBUG
+
+// Leak checking for MS Visual C++
+#ifdef _MSC_VER
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#define new DEBUG_NEW
+
+#endif // _MSC_VER
+
+#endif  // DEBUG
+
+#endif  // __DEBUG_H__
