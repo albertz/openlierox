01f3393f35c397e0e243f830acff07745a25165b
dark_charlie <dark_charlie@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-11-01 11:55:17 +0000

    Resources and icon for MSVC 2008 project
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@2800 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- build/msvc 2005/resource.h
+++ build/msvc 2005/resource.h
@@ -5,13 +5,5 @@
 #define IDI_LARGE                       101
 #define IDI_SMALL                       102
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+// Do not include StringUtils.h from Version.h, it causes errors on some compilers
+#define __STRINGUTILS_H__ 
