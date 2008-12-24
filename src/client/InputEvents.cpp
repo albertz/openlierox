1a592b96f5156be1f0d6bfb45b7a4f747d7557e7
pelya <pelya@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2008-12-24 13:37:23 +0000

    Reverted the hack
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@2935 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- src/client/InputEvents.cpp
+++ src/client/InputEvents.cpp
@@ -341,15 +341,6 @@ static void EvHndl_KeyDownUp(SDL_Event* ev) {
 			cout << tLX->fCurTime << ": released key " << kbev.sym << endl;
 		*/
 		
-		// If we have no focus and got keypress that means we missed ActiveEvent - set it forcefully
-		if( !nFocus )
-		{
-			SDL_Event activateEvent;
-			activateEvent.active.type = SDL_ACTIVEEVENT;
-			activateEvent.active.state = SDL_APPINPUTFOCUS | SDL_APPACTIVE;
-			activateEvent.active.gain = 1;
-			EvHndl_ActiveEvent(&activateEvent);
-		}
 	} else
 		printf("strange Event.key.state = %i\n", ev->key.state);
 
