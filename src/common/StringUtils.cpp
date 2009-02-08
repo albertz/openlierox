9cbab0776c2b61a965423d80abb0463bdb936993
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2009-02-08 18:14:49 +0000

    fixed wrong GetBaseFilename
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@3252 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- src/common/StringUtils.cpp
+++ src/common/StringUtils.cpp
@@ -527,17 +527,7 @@ std::string GetFileExtension(const std::string& path) {
 std::string GetBaseFilename(const std::string& filename) {
 	size_t p = findLastPathSep(filename);
 	if(p == std::string::npos) return filename;
-
-	std::string result = filename.substr(p+1);
-
-	// Strip parameters if any
-	p = result.find(' ');
-	size_t p2 = result.rfind('.');
-	if (p == std::string::npos || (p2 != std::string::npos && p2 > p))
-		return result;
-	result.erase(p);
-
-	return result;
+	return filename.substr(p+1);
 }
 
 
