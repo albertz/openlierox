821c290b0d0ae0cd50da31660af9782c75f75bd4
pelya <pelya@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2009-03-12 17:48:15 +0000

    Reverted X11 hacks
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@3559 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- src/client/NotifyUser.cpp
+++ src/client/NotifyUser.cpp
@@ -24,8 +24,40 @@ void ClearUserNotify() {}
 	#include <SDL.h>
 	#include <SDL_syswm.h>
 #elif defined(X11)
+	#include <X11/Xlib.h>
+	#include <SDL.h>
+	#include <SDL_syswm.h>
 
-extern "C" void x11_SetDemandsAttention( int v );
+void x11_SetDemandsAttention( bool v ) {
+	SDL_SysWMinfo info;
+	SDL_VERSION(&info.version);
+	SDL_GetWMInfo(&info);
+
+	info.info.x11.lock_func();
+
+	// they will stay always the same as long as X11 is running
+	static Atom demandsAttention = XInternAtom(info.info.x11.display, "_NET_WM_STATE_DEMANDS_ATTENTION", true);
+	static Atom wmState = XInternAtom(info.info.x11.display, "_NET_WM_STATE", true);
+
+	XEvent e;
+	e.xclient.type = ClientMessage;
+	e.xclient.message_type = wmState;
+	e.xclient.display = info.info.x11.display;
+	e.xclient.window = info.info.x11.wmwindow;
+	e.xclient.format = 32;
+	e.xclient.data.l[0] = v ? 1 : 0;
+	e.xclient.data.l[1] = demandsAttention;
+	e.xclient.data.l[2] = 0l;
+	e.xclient.data.l[3] = 0l;
+	e.xclient.data.l[4] = 0l;
+
+	if( v ) // ensure that the property is there (that is not guaranteed on all systems)
+		XChangeProperty(info.info.x11.display, info.info.x11.wmwindow, wmState, XA_ATOM, 32, PropModeReplace, (unsigned char *)&demandsAttention, 1);
+
+	XSendEvent(info.info.x11.display, DefaultRootWindow(info.info.x11.display), False, (SubstructureRedirectMask | SubstructureNotifyMask), &e);
+
+	info.info.x11.unlock_func();
+}
 
 #endif
 
@@ -81,7 +113,7 @@ void NotifyUserOnEvent()
 
 #elif defined(X11)
 
-	x11_SetDemandsAttention(1);
+	x11_SetDemandsAttention(true);
 
 #else
 
@@ -96,7 +128,7 @@ void ClearUserNotify() {
 #if defined(__APPLE__)
 #elif defined(WIN32)
 #elif defined(X11)
-	x11_SetDemandsAttention(0);
+	x11_SetDemandsAttention(false);
 #endif
 }
 
