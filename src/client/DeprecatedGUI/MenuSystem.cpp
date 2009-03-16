bf572d189f7eb6a02bfe10be7c2bf2bac7faf5f5
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2009-03-16 18:49:42 +0000

    small fix
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@3615 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- src/client/DeprecatedGUI/MenuSystem.cpp
+++ src/client/DeprecatedGUI/MenuSystem.cpp
@@ -627,9 +627,11 @@ MessageBoxReturnType Menu_MessageBox(const std::string& sTitle, const std::strin
 {
 	if(bDedicated) {
 		hints << "Menu_MessageBox: " << sTitle << ": " << sText << endl;
-		if(type == LMB_YESNO) {
-			hints << "Dedicated server is positive and says YES." << endl;
-			return MBR_YES;
+		switch(type) {
+			case LMB_OK: return MBR_OK;
+			case LMB_YESNO:
+				hints << "Dedicated server is positive and says YES." << endl;
+				return MBR_YES;
 		}
 		return MBR_OK;
 	}
