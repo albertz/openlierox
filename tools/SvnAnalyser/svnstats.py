8ebc90ba857cb045a25a7ae49570b6cf4b11e296
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2009-03-28 02:23:23 +0000

    some fixes
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@3839 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- tools/SvnAnalyser/svnstats.py
+++ tools/SvnAnalyser/svnstats.py
@@ -8,6 +8,13 @@ import StringIO
 import gzip
 import getopt
 
+dirs = ["src", "include"]
+
+svnrepo = "."
+# just some check for my server
+if os.path.isdir("/backup/data/openlierox-svn"):
+	svnrepo = "file://localhost/backup/data/openlierox-svn"
+
 os.chdir(os.path.dirname(sys.argv[0]) + "/../..")
 
 startrev = 2 # excluding initial commit, that's not our work :)
@@ -29,7 +36,7 @@ def svnInfo(revision):
 	comment = str()
 	author = str()
 	date = str()
-	for l in os.popen("svn log -r %i" % revision).read().splitlines():
+	for l in os.popen("svn log -r %i %s" % (revision, svnrepo)).read().splitlines():
 		if len(l) == 0: continue
 		if l.startswith("------------"): continue
 		if first:
@@ -52,18 +59,23 @@ def analyseData(preprint, data):
 	print preprint, "compress rate:", (100 * len(data) / complen), "%"
 	return min(len(data), complen)
 
-def analyseDiff(diffOut):
-	addedLines = str()
-	removedLines = str()
+def analyseDiff(dir, diffOut):
+	changedLines = str()
+	file = ""
 	for l in diffOut.splitlines():
 		if len(l) == 0: continue
+		if l.startswith("======="): continue
+		if l.startswith("Index: "):
+			file = dir + "/" + l[7:]
+			print "  file:", file
+			continue
+		if file == "": continue
 		if l.startswith("+"):
-			addedLines += l[1:] + "\n"
-		if l.startswith("-"):
-			removedLines += l[1:] + "\n"
-	addLen = analyseData("    added:", addedLines)
-	remLen = analyseData("    removed:", removedLines)
-	return addLen - remLen
+			changedLines += l[1:] + "\n"
+		#if l.startswith("-"):
+		#	changedLines += l[1:] + "\n"
+	changedLen = analyseData("    ", changedLines)
+	return changedLen
 
 
 stats = dict()
@@ -72,12 +84,15 @@ def analyseSvnRev(rev):
 	print "rev", rev, ":",
 	info = svnInfo(rev)
 	print info
-	diffOut = os.popen("svn diff -r %i:%i -x --ignore-eol-style" % (rev - 1, rev)).read()
-	res = analyseDiff(diffOut)
 	if not info[0] in stats:
-		print "new dev:", info[0]
+		print "  new dev:", info[0]
 		stats[info[0]] = 0
-	stats[info[0]] += max(0, res)
+
+	for d in dirs:
+		diffOut = os.popen("svn diff -r %i:%i -x --ignore-eol-style %s/%s" % (rev - 1, rev, svnrepo, d)).read()
+		res = analyseDiff(d, diffOut)
+		stats[info[0]] += max(0, res)
+
 	return True
 
 rev = startrev
