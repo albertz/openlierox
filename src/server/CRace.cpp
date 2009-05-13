a5f061f699cf6a91f089d9ff2ce791b4254a2117
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2009-05-13 16:06:08 +0000

    better score sorting in Race mode
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@4336 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- src/server/CRace.cpp
+++ src/server/CRace.cpp
@@ -30,6 +30,7 @@ struct Race : public CGameMode {
 	
 	Race() { reset(); }
 	
+	static const unsigned int WPNUM = 4;
 	std::vector<CVec> wayPoints;
 	
 	std::string flagName(int t) {
@@ -48,7 +49,7 @@ struct Race : public CGameMode {
 		CGameMode::PrepareGame();
 		reset();
 		
-		wayPoints.resize(4);
+		wayPoints.resize(WPNUM);
 		for(int x = 0; x <= 1; x++)
 			for(int y = 0; y <= 1; y++) {
 				std::list<CVec> goodPos;
@@ -115,6 +116,14 @@ struct Race : public CGameMode {
 		sendWormScoreUpdate(w);
 	}
 	
+	virtual std::string TeamNameForWorm(CWorm* w) {
+		return w->getName();
+	}
+	
+	virtual int Round(CWorm* w) {
+		return w->getKills();
+	}
+	
 	virtual void hitFlagSpawnPoint(CWorm* worm, Flag* flag) {
 		if(getWormFlag(worm) == flag->id) {
 			if(flag->holderWorm == worm->getID())
@@ -122,9 +131,13 @@ struct Race : public CGameMode {
 			else
 				cServer->flagInfo()->applyHolderWorm(flag, worm->getID());
 			
-			nextGoals[getWormFlag(worm)] = (nextGoals[getWormFlag(worm)] + 1) % 4;
+			nextGoals[getWormFlag(worm)] = (nextGoals[getWormFlag(worm)] + 1) % WPNUM;
 			cServer->flagInfo()->applySpawnPos(flag, wayPoints[nextGoals[getWormFlag(worm)]]);
 
+			if(nextGoals[getWormFlag(worm)] == 1)
+				cServer->SendGlobalText(TeamNameForWorm(worm) + " is in round " + itoa(Round(worm)), TXT_NORMAL);
+			
+			
 			if(nextGoals[getWormFlag(worm)] == 1) {
 				// we made one round
 				addScore(worm);
@@ -137,7 +150,7 @@ struct Race : public CGameMode {
 		// get that information from client because both client&server can check this
 		return cClient->getGameLobby()->features[FT_Race_AllowWeapons];
 	}
-
+	
 	virtual bool CheckGameOver() {
 		if(int(tLXOptions->tGameInfo.features[FT_Race_Rounds]) > 0) {
 			for(int i = 0; i < MAX_WORMS; ++i) {
@@ -172,6 +185,52 @@ struct Race : public CGameMode {
 		
 		return false;
 	}
+
+	struct CompareGoals {
+		Race* race;
+		CompareGoals(Race* r) : race(r) {}
+		
+		int comp(CWorm* w1, CWorm* w2) {
+			{
+				// compare next goals
+				int lastGoal1 = (race->nextGoals[race->getWormFlag(w1)] + WPNUM - 1) % WPNUM;
+				int lastGoal2 = (race->nextGoals[race->getWormFlag(w2)] + WPNUM - 1) % WPNUM;
+				if(lastGoal1 > lastGoal2) return 1;
+				if(lastGoal1 < lastGoal2) return -1;
+			}
+			
+			{
+				// compare distance to next goals (they should be the same for both worms)
+				float dist1 = (race->wayPoints[race->nextGoals[race->getWormFlag(w1)]] - w1->getPos()).GetLength2();
+				float dist2 = (race->wayPoints[race->nextGoals[race->getWormFlag(w2)]] - w2->getPos()).GetLength2();
+				if(dist1 < dist2) return 1; // it's better to be more close
+				if(dist1 > dist2) return -1;
+			}
+			
+			return 0;			
+		}
+		
+		bool operator()(CWorm* w1, CWorm* w2) {
+			return comp(w1, w2) > 0;
+		}
+	};
+	
+	int CompareWormsScore(CWorm* w1, CWorm* w2) {
+		// Kills very first (that is the amount of rounds)
+		if(tLXOptions->tGameInfo.iKillLimit > 0) {
+			if (w1->getKills() > w2->getKills()) return 1;
+			if (w1->getKills() < w2->getKills()) return -1;		
+		}
+
+		{
+			// compare goals; we can only do that if we are server, because we don't have the waypoints otherwise
+			int d = (cServer && cServer->getWorms()) ? CompareGoals(this).comp(w1, w2) : 0;
+			if(d != 0) return d;
+		}
+		
+		// fallback (very unprobable that this will be used)
+		return CGameMode::CompareWormsScore(w1, w2);
+	}
 };
 
 struct TeamRace : public Race {
@@ -204,7 +263,9 @@ struct TeamRace : public Race {
 	virtual void addScore(CWorm* w) {
 		teamScore[CLAMP(w->getTeam(),0,MAXTEAMS-1)]++;
 		cServer->SendTeamScoreUpdate();
-		//cServer->SendGlobalText(worm->getName() + " scored for " + TeamName(worm->getTeam()), TXT_NORMAL);
+		// also add score for this specifc worm
+		w->AddKill();
+		sendWormScoreUpdate(w);
 	}
 	
 	virtual int getWormFlag(CWorm* worm) {
@@ -214,16 +275,50 @@ struct TeamRace : public Race {
 	bool isTeamUsed(int t) {
 		return !cServer->isTeamEmpty(t);
 	}
+
+	virtual std::string TeamNameForWorm(CWorm* w) {
+		return TeamName(w->getTeam());
+	}
+	
+	virtual int Round(CWorm* w) {
+		return TeamScores(w->getTeam());
+	}
 	
 	virtual int TeamScores(int t) {
 		if(t >= 0 && t < MAXTEAMS) return teamScore[t];
 		return -1;
 	}
 	
+	std::vector<CWorm*> getTeamWorms(int t) {
+		std::vector<CWorm*> worms;
+		if(cServer->getWorms() == NULL) return worms;
+		for(int i = 0; i < MAX_WORMS; i++)
+			if(cServer->getWorms()[i].isUsed())
+				if(cServer->getWorms()[i].getTeam() != t)
+					worms.push_back(&cServer->getWorms()[i]);
+		return worms;
+	}
+	
+	CWorm* getTeamBestWorm(int t) {
+		std::vector<CWorm*> teamWorms = getTeamWorms(t); 
+		if(teamWorms.size() == 0) return NULL;
+		std::sort(teamWorms.begin(), teamWorms.end(), CompareGoals(this));
+		return teamWorms.front();
+	}
+	
 	virtual int CompareTeamScore(int t1, int t2) {
 		// team score is most important in CTF, more important than left lifes
 		{
 			int d = TeamScores(t1) - TeamScores(t2);
+			if(d != 0) return d;
+		}
+
+		{
+			// compare the goals of the best worms of each team
+			CWorm* w1 = getTeamBestWorm(t1);
+			CWorm* w2 = getTeamBestWorm(t2);
+			
+			int d = (w1 && w2) ? CompareGoals(this).comp(w1, w2) : 0;
 			if(d != 0) return d;
 		}
 		
