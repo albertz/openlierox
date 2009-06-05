b05bb359ed5391b08ed94548dae359b64aa37367
pelya <pelya@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2009-06-05 12:09:36 +0000

    Auto-recompile .EXE only if you write string "[rebuild]" in commit logs.
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@4546 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- distrib/nightlyCompileOlx.bat
+++ distrib/nightlyCompileOlx.bat
@@ -1,3 +1,6 @@
+@rem This script requires Cmake, grep and svn command-line tools
+@rem Ideally it requires Bash and Cygwin, but that's optional
+
 echo on
 if exist x:\compileOlx.flag goto compile
 
@@ -7,13 +10,19 @@ exit
 
 del x:\compileOlx.flag
 
-echo Building OLX at %DATE% > x:\nightlyCompileOlx.log
+echo ----------- Nightly build started at %DATE% > x:\nightlyCompileOlx.log
 
 call "C:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat" x86
 echo on
 
 cd c:\openlierox
+svn log -r "BASE:HEAD" > svn.log
 svn up
+grep -i "\[Rebuild\]" svn.log
+
+if errorlevel 1 goto end
+
+echo ----------- Compiling >> x:\nightlyCompileOlx.log
 
 cd "build\msvc 2005"
 
@@ -26,17 +35,18 @@ vcbuild openlierox.vcproj "RelWithDebInfo|Win32" /useenv /showenv >> x:\nightlyC
 
 if exist ..\..\distrib\win32\OpenLieroX.exe goto commit
 
-echo Failed to build OLX >> x:\nightlyCompileOlx.log
+echo ----------- Compiling failed >> x:\nightlyCompileOlx.log
 
 goto end
 
 :commit
 
+echo ----------- Committing to SVN >> x:\nightlyCompileOlx.log
+
 cd ..\..
 
 svn commit -m "Updated EXE (auto nightly build)"
 
-rem Requires Cygwin!
 bash -c "echo `grep -o 'r[0-9]*' optional-includes/generated/Version_generated.h` > revnum.txt"
 
 bash -c "cmd /c save_debug_info.bat `cat revnum.txt`"
@@ -44,5 +54,7 @@ bash -c "zip -r debuginfo_`cat revnum.txt`.zip debuginfo/`cat revnum.txt`"
 rem bash -c "echo Debug symbols attached | email --attach debuginfo_`cat revnum.txt`.zip --subject 'Debug symbols automated mail' --smtp-server 172.17.57.25 karel.petranek@tiscali.cz"
 
 :end
+
+echo ----------- Nightly build finished at %DATE% >> x:\nightlyCompileOlx.log
 
 shutdown -s now
