27855cd5870710dc1525ce26f86f54d8b4f76ec3
dark_charlie <dark_charlie@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2009-08-05 14:35:45 +0000

    Updated script to generate coredumps
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@4818 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- distrib/scanWin32Coredumps.sh
+++ distrib/scanWin32Coredumps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd ..
+debuginfodir="../../debuginfo"
+
+separator="-------SEPARATOR--------"
 
 for dump in coredumps/*.dmp; do
 
@@ -10,16 +12,21 @@ for dump in coredumps/*.dmp; do
 	fi
 	if [ -z "$rev" ]; then
 		echo Coredump $dump rev `Win32MinidumpAnalyzer.exe "$dump" 0` - cannot determine revnumber > "$dump.txt"
+		echo $separator >> "$dump.txt"
 		continue
 	fi
 	echo Coredump $dump rev $rev > "$dump.txt"
+	echo $separator >> "$dump.txt"
 	Win32MinidumpAnalyzer.exe "$dump" 1 >> "$dump.txt"
+	echo $separator >> "$dump.txt"
 	Win32MinidumpAnalyzer.exe "$dump" 2 >> "$dump.txt"
-	if [ \! -e "debuginfo/$rev" ] ; then
+	echo $separator >> "$dump.txt"
+	
+	if [ \! -e "$debuginfodir/$rev" ] ; then
 		echo No debuginfo for rev $rev >> "$dump.txt"
 		revnumActual=`echo $rev | grep -E -o '[0-9]+'`
 		revnumMinDiff=100000000
-		for f in `ls debuginfo` ; do
+		for f in `ls $debuginfodir` ; do
 			revnumCheck=`echo $f | grep -E -o '[0-9]+'`
 			revnumDiff=`expr $revnumCheck - $revnumActual | sed 's/-//'`
 			if [ $revnumDiff -lt $revnumMinDiff ] ; then
@@ -27,35 +34,41 @@ for dump in coredumps/*.dmp; do
 				rev=$f
 			fi
 		done
-		if [ \! -e "debuginfo/$rev" ] ; then
+		if [ \! -e "$debuginfodir/$rev" ] ; then
 			echo Cannot find any debuginfo for rev $rev >> "$dump.txt"
+			echo $separator >> "$dump.txt"
 			continue
 		fi
 		echo Found closest matching debuginfo - rev $rev >> "$dump.txt"
+		echo $separator >> "$dump.txt"
 	fi
 	
-	cdb -y "debuginfo/$rev" -i "debuginfo/$rev" -z "$dump" \
-		-srcpath "debuginfo/$rev/sources" -lines \
+	cdb -y "$debuginfodir/$rev" -i "$debuginfodir/$rev" -z "$dump" \
+		-srcpath "$debuginfodir/$rev/sources" -lines \
 		-c ".kframes 100; .ecxr; kp; q" > except.txt
 	fileline=`grep -o -m 1 '[^[]*@ [0-9]*' except.txt`
 	rm except.txt
 
 	if [ -z "$fileline" ]; then
 		echo Cannot determine exception location in source files >> "$dump.txt"
-		cdb -y "debuginfo/$rev" -i "debuginfo/$rev" -z "$dump" \
-			-srcpath "debuginfo/$rev/sources" -lines \
-			-c ".kframes 100; .ecxr; kp; ~*kp; q" >> "$dump.txt"
+		echo $separator >> "$dump.txt"
+		cdb -y "$debuginfodir/$rev" -i "$debuginfodir/$rev" -z "$dump" \
+			-srcpath "$debuginfodir/$rev/sources" -lines \
+			-c ".kframes 100; .ecxr; kp; ~*kp; q" >> "$dump.txt",
+		echo $separator >> "$dump.txt"
 	else
 		file=`echo $fileline | grep -o '[^@]*' | sed 's/ //'`
 		line=`echo $fileline | grep -o '@ [0-9]*' | sed 's/@ //'`
 		echo Exception at $file: $line >> "$dump.txt"
+		echo $separator >> "$dump.txt"
 		if [ $line -gt 10 ]; then
 			line=`expr $line - 10`
 		else
 			line=0
 		fi
-		cdb -y "debuginfo/$rev" -i "debuginfo/$rev" -z "$dump" \
-			-srcpath "debuginfo/$rev/sources" -lines \
+		cdb -y "$debuginfodir/$rev" -i "$debuginfodir/$rev" -z "$dump" \
+			-srcpath "$debuginfodir/$rev/sources" -lines \
 			-c ".kframes 100; lsp -a 14; lsf $file; ls $line; .ecxr; kp; ~*kp; q" >> "$dump.txt"
+		echo $separator >> "$dump.txt"
 	fi
 done
