3308a04b0fd1508efc6f873ac28eec117829d9cb
albertzeyer <albertzeyer@34602234-ff1f-0410-a465-ea8f3b77ab7f>
2009-09-01 16:31:26 +0000

    small fix
    git-svn-id: https://openlierox.svn.sourceforge.net/svnroot/openlierox@4875 34602234-ff1f-0410-a465-ea8f3b77ab7f

--- src/client/DeprecatedGUI/Menu_Net_Host.cpp
+++ src/client/DeprecatedGUI/Menu_Net_Host.cpp
@@ -1337,7 +1337,7 @@ void Menu_HostDrawLobby(SDL_Surface * bmpDest)
 {
 	CListview *player_list = (CListview *)cHostLobby.getWidget(hl_PlayerList);
 	if (!player_list) { // Weird, shouldn't happen
-		warnings << "Menu_HostDrawLobby: player_list not set" << endl;
+		errors << "Menu_HostDrawLobby: player_list not set" << endl;
 		return;
 	}
 
