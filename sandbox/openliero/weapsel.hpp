964e3b0362a1066ccbca699374c8f8f703eab38d
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2009-10-05 01:35:08 +0200

    we dont need openliero nor lua sandbox stuff in 0.58

--- sandbox/openliero/weapsel.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef LIERO_WEAPSEL_HPP
-#define LIERO_WEAPSEL_HPP
-
-void selectWeapons();
-void selectWeaponsInit_OlxMod_01();
-void selectWeaponsLoop_OlxMod_01();
-
-#endif // LIERO_WEAPSEL_HPP
