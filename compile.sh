122d777eea165be3838ce6aa1392993d683722f1
pelya <x.pelya.x@gmail.com>
2009-10-05 16:53:15 +0300

    Use fixed compile.sh and functions.sh from master branch

--- compile.sh
+++ compile.sh
@@ -19,12 +19,15 @@
 #					( disabled by default )
 #	X11				- if set to 1, X11 clipboard/notifying will be used (and linked against libX11)
 #					( activated by default )
-#
 #	G15				- if set to 1, G15 support will be builtin (and linked against required libraries)
+#					( disabled by default )
+#	GCOREDUMPER		- if set to 1, Google Coredumper is used
 #					( disabled by default )
 #	VERSION			- version number; like 0.57_beta2
 #					  if not set, the function functions.sh:get_olx_version
 #					  generates the string automatically
+
+cd "$(dirname "$0")"
 
 . ./functions.sh
 
@@ -35,6 +38,7 @@
 [ "$ACTIVATE_GDB" = "" ] && [ "$DEBUG" = "1" ] && ACTIVATE_GDB=1
 [ "$X11" = "" ] && X11=1
 [ "$G15" = "" ] && G15=0
+[ "$GCOREDUMPER" = "" ] && GCOREDUMPER=0
 [ "$VERSION" = "" ] && VERSION=$(get_olx_version)
 
 # add standards to include path list
@@ -51,10 +55,10 @@ test_exec pkg-config && pkg-config libxml-2.0 && xmlconfig="pkg-config libxml-2.
 [ "$xmlconfig" = "" ] && test_exec xml2-config && xmlconfig="xml2-config"
 [ "$xmlconfig" = "" ] && xmlconfig="own_xml2_config"
 
-INCLUDE_PATH="$INCLUDE_PATH $($sdlconfig --cflags | grep_param() -I)"
-LIB_PATH="$LIB_PATH $($sdlconfig --libs | grep_param() -L)"
-INCLUDE_PATH="$INCLUDE_PATH $($xmlconfig --cflags | grep_param() -I)"
-LIB_PATH="$LIB_PATH $($xmlconfig --libs | grep_param() -L)"
+INCLUDE_PATH="$INCLUDE_PATH $($sdlconfig --cflags | grep_param -I)"
+LIB_PATH="$LIB_PATH $($sdlconfig --libs | grep_param -L)"
+INCLUDE_PATH="$INCLUDE_PATH $($xmlconfig --cflags | grep_param -I)"
+LIB_PATH="$LIB_PATH $($xmlconfig --libs | grep_param -L)"
 
 echo "--- OpenLieroX compile.sh ---"
 
@@ -144,6 +148,9 @@ echo "* $COMPILER will be used for compilation"
 [ "$LIBZIP_BUILTIN" = "1" ] && \
 	echo "* libzip support will be built into the binary" || \
 	echo "* the binary will be linked dynamically against libzip"
+[ "$GCOREDUMPER" = "1" ] && \
+	echo "* Googles coredumper will be used" || \
+	echo "* debuggers coredumper will be used"
 
 
 
@@ -153,9 +160,11 @@ if $COMPILER \
 	src/*.cpp src/client/*.cpp \
 	src/client/DeprecatedGUI/*.cpp src/client/SkinnedGUI/*.cpp \
 	src/common/*.cpp src/server/*.cpp \
+	libs/coredumper/src/*.c \
 	$LIBZIP_GCC_PARAM \
 	$HAWKNL_GCC_PARAM \
 	-I include -I libs/pstreams \
+	-I libs/coredumper/src \
 	$(build_param_str -I "$INCLUDE_PATH" "/. /libxml2 /hawknl") \
 	$(build_param_str -L "$LIB_PATH") \
 	$($sdlconfig --cflags) \
@@ -169,6 +178,7 @@ if $COMPILER \
 	$( [ "$ACTIVATE_GDB" = "1" ] && echo "-g" ) \
 	$( [ "$X11" = "1" ] && echo "-DX11 -lX11" ) \
 	$( [ "$G15" = "1" ] && echo "-DWITH_G15 -lg15daemon_client -lg15render" ) \
+	$( [ "$GCOREDUMPER" = "1" ] && echo "-DGCOREDUMPER -Ilibs/coredumper/src" ) \
 	$CXXFLAGS \
 	$LDFLAGS \
 	-o bin/openlierox
