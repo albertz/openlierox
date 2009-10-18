313f73942e92f29cbca9100658067021c8d23cf4
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2009-10-18 17:11:51 +0200

    extended crash report a bit

--- tools/CrashReporter/SimpleCrashReporter.py
+++ tools/CrashReporter/SimpleCrashReporter.py
@@ -3,13 +3,14 @@
 import sys, smtplib
 import os
 
-if len(sys.argv) < 3:
-	print "Usage: " + sys.argv[0] + " <dumpdir> <minidumpid>"
+if len(sys.argv) < 4:
+	print "Usage: " + sys.argv[0] + " <dumpdir> <minidumpid> <productname>"
 	exit(1)
 
 appdir = os.path.dirname(sys.argv[0])
 dumpfile = sys.argv[1] + "/" + sys.argv[2] + ".dmp"
 reporter = appdir + "/crash_report"
+productname = sys.argv[3]
 
 if not os.path.exists(reporter):
 	print "Reporter binary crash_report does not exist"
@@ -20,8 +21,17 @@ if not os.path.exists(dumpfile):
 	exit(1)
 
 
-msg = "To: openlierox@az2000.de\r\nSubject: OpenLieroX crash report\r\n"
-msg = msg + "Crash information:\n\n"
+msg = "To: openlierox@az2000.de\r\n"
+msg = msg + "Subject: " + productname + " crash report\r\n"
+msg = msg + "\r\n"
+
+msg = msg + "System information:\n\n"
+
+msg = msg + "os.name = " + os.name + "\n"
+msg = msg + "os.environ = " + os.environ.__repr__() + "\n"
+msg = msg + "os.uname = " + os.uname().__repr__() + "\n"
+
+msg = msg + "\n\n\nCrash information:\n\n"
 
 stream = os.popen("\"" + reporter + "\" \"" + dumpfile + "\" 2>/dev/null")
 
