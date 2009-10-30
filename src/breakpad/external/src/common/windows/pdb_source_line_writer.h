ea6a19f4410f5b00908994e304c5b95b9b3decd8
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2009-10-30 01:54:43 +0100

    fixes for windows/mscs/dia/atl/whatever to compile

--- src/breakpad/external/src/common/windows/pdb_source_line_writer.h
+++ src/breakpad/external/src/common/windows/pdb_source_line_writer.h
@@ -33,8 +33,7 @@
 #ifndef _PDB_SOURCE_LINE_WRITER_H__
 #define _PDB_SOURCE_LINE_WRITER_H__
 
-//#include "CComPtr.h"
-#include <atlbase.h>
+#include <atlcomcli.h>
 
 #include <string>
 
@@ -43,8 +42,6 @@ struct IDiaSession;
 struct IDiaSymbol;
 
 namespace google_breakpad {
-
-using ATL::CComPtr;
 
 using std::wstring;
 
