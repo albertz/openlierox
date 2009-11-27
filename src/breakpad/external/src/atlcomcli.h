3601d5a4d140d16f89ebb1bc442ad25c65e6bfea
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2009-11-27 21:15:09 +0100

    hack doesnt work, it uses that hack file instead of the real

--- src/breakpad/external/src/atlcomcli.h
+++
@@ -1,3 +0,0 @@
-// HINT: this is a fake file which is not included if the system file exists
-// The compiler will include this one only if the system file does not exist which means we are most probably on a MSVC Express edition and will activate a workaround
-#define MSVC_EXPRESS_FIX
