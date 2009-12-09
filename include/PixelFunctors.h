12889475e353370ca5b3602eb5c4b80a0ec89c8a
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2009-12-09 22:15:58 +0100

    fixed pixel drawing functions

--- include/PixelFunctors.h
+++ include/PixelFunctors.h
@@ -172,7 +172,7 @@ class PixelGet_16 : public PixelGet {
 inline Uint32 GetPixel_24(const Uint8 *addr) {
 		union { Uint8 u8[4]; Uint32 u32; } col;
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-		col.u8[0] = 0;
+		col.u8[0] = 255;
 		col.u8[1] = addr[2];
 		col.u8[2] = addr[1];
 		col.u8[3] = addr[0];
@@ -180,7 +180,7 @@ inline Uint32 GetPixel_24(const Uint8 *addr) {
 		col.u8[0] = addr[0];
 		col.u8[1] = addr[1];
 		col.u8[2] = addr[2];
-		col.u8[3] = 0;
+		col.u8[3] = 255;
 #endif
 		return col.u32;
 }
@@ -196,6 +196,12 @@ inline Uint32 GetPixel_32(const Uint8 *addr)  {
 
 class PixelGet_32 : public PixelGet {
 	Uint32 get(Uint8 *addr)	{ return GetPixel_32(addr); }
+};
+
+class PixelGet_32_nonalpha : public PixelGet {
+	Uint32 get(Uint8 *addr)	{
+		return Uint32(0xff000000) | GetPixel_32(addr);
+	}
 };
 
 
@@ -412,7 +418,8 @@ inline PixelGet& getPixelGetFunc(const SDL_Surface *surf)  {
 	static PixelGet_16 px16;
 	static PixelGet_24 px24;
 	static PixelGet_32 px32;
-
+	static PixelGet_32_nonalpha px32nonalpha;
+	
 	switch (surf->format->BytesPerPixel)  {
 	case 1:
 		return px8;
@@ -421,7 +428,8 @@ inline PixelGet& getPixelGetFunc(const SDL_Surface *surf)  {
 	case 3:
 		return px24;
 	case 4:
-		return px32;
+		if(surf->format->Amask != 0) return px32;
+		else return px32nonalpha;
 	default:
 		assert(false);
 	}
