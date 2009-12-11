ebbdb79f5d0e290ab7aef7b8a7f600b982ab662d
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2009-12-11 15:03:12 +0100

    small comment on why we need the dummy physics engine

--- include/PhysicsDummy.h
+++ include/PhysicsDummy.h
@@ -13,6 +13,17 @@
 #include "Physics.h"
 #include <string>
 
+/*
+The dummy physics engine does nothing itself.
+
+It is used right now for Gusanos because we call the gusanos logic frame handler
+independently from the physics interface in each frame (because the gusanos logic
+frame handler may also do other things independently (scripted maps), so we need
+it in any case, even with LX56 physics).
+
+Thus, gusanos don't need extra handling from the physics interface.
+*/
+
 PhysicsEngine* CreatePhysicsEngineDummy(const std::string& enginename = "dummy physics engine");
 
 #endif
