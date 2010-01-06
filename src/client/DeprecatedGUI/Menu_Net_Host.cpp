8a66f509458fe8cb600fe4346c04f5507e5aa1cf
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-01-06 17:40:21 +0100

    missing bAutoFileCacheRefresh check

--- src/client/DeprecatedGUI/Menu_Net_Host.cpp
+++ src/client/DeprecatedGUI/Menu_Net_Host.cpp
@@ -901,7 +901,7 @@ void Menu_Net_HostLobbyFrame(int mouse)
 
 	// Reload the level and mod list when the user switches back to game
 	// Do not reaload when any of the dialogs is open
-	if (bActivated)  {
+	if (bActivated && tLXOptions->bAutoFileCacheRefresh)  {
 		notes << "Switched back to OLX, re-filling the level and mod lists" << endl;
 
 		// Get the mod name
