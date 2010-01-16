1593b6b999263def46fd61c177a9c9d628b6f559
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-01-16 03:30:38 +0100

    beginning/demo of games

---
+++ share/gamedir/games/introduction/intro1/scripts/map_intro1.lua
@@ -0,0 +1,13 @@
+function init_intro1()
+	exec("chatMsg", "Hello from the introduction level")
+
+	message("Welcome to OpenLieroX\n"
+		.. "Go to the right top of this level!\n"
+		.. "Use the ninja rope (key " .. getVar("GameOptions.Ply1Controls.Rope") .. ")")
+
+end
+
+function goalhit()
+	setLevelSucceeded()
+end
+
