51140ed2bfb9ce88bdd3a00caa4b9d32b8042f6b
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-01-17 13:33:46 +0100

    improved intro1 a bit

--- share/gamedir/games/introduction/intro1/scripts/map_intro1.lua
+++ share/gamedir/games/introduction/intro1/scripts/map_intro1.lua
@@ -1,9 +1,18 @@
+function getKey(k)
+	return getVar("GameOptions.Ply1Controls." .. k)
+end
+
 function init_intro1()
 	exec("chatMsg", "Hello from the introduction level")
 
 	message("Welcome to OpenLieroX\n"
-		.. "Go to the right top of this level!\n"
-		.. "Use the ninja rope (key " .. getVar("GameOptions.Ply1Controls.Rope") .. ")")
+		.. "Go to the right top of this level!\n\n"
+		.. "You can move left/right (keys " .. getKey("Left") .. "/" .. getKey("Right") .. ").\n"
+		.. "You can jump (key " .. getKey("Jump") .. ").\n"
+		.. "And you should use the ninja rope :) (key " .. getKey("Rope") .. ").\n"
+		.. "Release the ninja by jumping (key " .. getKey("Jump") .. ").\n"
+		.. "Don't forget to aim (keys " .. getKey("Up") .. "/" .. getKey("Down") .. ")."
+		)
 
 end
 
