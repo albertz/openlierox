a60e8c898d53c689ce6d79e9b7bab4147b600b33
pelya <x.pelya.x@gmail.com>
2010-02-01 11:24:58 +0000

    Installer script for ded video recorder

---
+++ tools/DedicatedServerVideo/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# This script will create symlinks to correct places in current OLX GIT directory structure,
+# so you'll be able to start it with "./start.sh -dedicated" from root dir and get video recording out of the box.
+# If you'll move git dir to other place just run the script again
+
+ln -s -f `pwd`/YoutubeUpload.py `pwd`/atom `pwd`/gdata ../../share/gamedir/
+ln -s -f `pwd`/dedicated-video-record.sh ../../share/gamedir/scripts
