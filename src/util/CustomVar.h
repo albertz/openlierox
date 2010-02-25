b09bd717a0756e9a3d5d7f7ebb28fcf1ec55a421
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-02-25 13:25:57 +0100

    small fix

--- src/util/CustomVar.h
+++ src/util/CustomVar.h
@@ -16,7 +16,7 @@
 // for custom variables types (not just string/bool/int/float)
 // This can be used in the scriptable vars system.
 struct CustomVar {
-	typedef Ref<CustomVar> Ref;
+	typedef ::Ref<CustomVar> Ref;
 	virtual ~CustomVar() {}
 	virtual CustomVar* copy() const = 0;
 	virtual bool operator==(const CustomVar&) const = 0;
