04b853c1aca93f03c90f55ab44e8286215220f6a
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-03-27 18:52:22 +0000

    ded script simulator

---
+++ share/gamedir/scripts/simulator.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python -u
+
+import sys, os, re
+
+if len(sys.argv) < 2:
+	print "usage:", sys.argv[0], "<script>"
+	exit(1)
+
+script = sys.argv[1]
+sin, sout = os.popen2(script)
+
+def getvar(var):
+	var = var.lower()
+
+	if var == "gameoptions.network.forceminversion":
+		return "OpenLieroX/0.58_rc1"
+
+	# no special handling yet; assume its a string and return empty
+	return ""
+
+
+def handle(cmd, params):
+	if cmd == "getwritefullfilename" or cmd == "getfullfilename":
+		return params # no translation
+
+	if cmd == "listmaps":
+		# some scripts need some output here
+		return ["CastleStrike.lxl"]
+
+	if cmd == "getvar":
+		return [ getvar(params[0]) ]
+
+	if cmd == "nextsignal":
+		while True:
+			ret = re.findall("[^ \t\"]+", raw_input("Enter signal: ").strip())
+			if len(ret) > 0: break
+		return ret
+
+	# unknown/not handled, just return empty list
+	return []
+
+
+while True:
+	l = sout.readline().strip()
+	print "Script:", l
+	cmd = re.findall("[^ \t\"]+", l)
+
+	if len(cmd) > 0:
+		ret = []
+		try:
+			ret = handle(cmd[0].lower(), cmd[1:])
+		except:
+			print "Error while handling", cmd
+			print sys.exc_info()
+		
+		for rl in ret:
+			sin.write(":" + str(rl) + "\n")
+		sin.write(".\n")
+		sin.flush()
+
+	else:
+		break
