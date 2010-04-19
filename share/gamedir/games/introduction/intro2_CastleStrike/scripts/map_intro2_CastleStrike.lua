c42b89937ba64aeb4d72adfeb2ea9b772824313c
pelya <x.pelya.x@gmail.com>
2010-04-19 17:36:56 +0300

    Updated introduction, added "How to combo" instructions

---
+++ share/gamedir/games/introduction/intro2_CastleStrike/scripts/map_intro2_CastleStrike.lua
@@ -0,0 +1,16 @@
+function getKey(k)
+	return getVar("GameOptions.Ply1Controls." .. k)
+end
+
+function map_intro2_CastleStrike.init()
+
+	message("Kill that bot 10 times, and avoid his shots.\n\n"
+		.. "An essential skill to do this is 'comboing': "
+		.. "hold your weapon selection key (" .. getKey("SelectWeapon") .. "),\n"
+		.. "then press Shoot key (" .. getKey("Shoot") .. ") and "
+		.. "quickly press Left or Right keys four times (" .. getKey("Left") .. "/" .. getKey("Right") .. ").\n"
+		.. "This will result in a deadly burst of five shots!\n"
+		.. "Move or aim up/down while shooting, and you'll cover larger area with shots."
+		)
+
+end
