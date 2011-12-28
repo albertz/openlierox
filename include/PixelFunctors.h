35efcbf3ad0ef59f23697c62e40620cbada581c9
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-12-28 23:39:03 +0100

    move some code and dont inline. this will anyway not improve from inlining
    it will not improve from inlining because it *dynamically* selects some function and then you call that selected function. at that point, the compiler cannot say anything about the function because it is selected dynamically.

--- include/PixelFunctors.h
+++ include/PixelFunctors.h
@@ -324,7 +324,7 @@ public:
 		issameformat,alphablend,colorkeycheck,srchasalpha,dsthasalpha,srcbytespp,dstbytespp
 		>(dfmt, sfmt, dstaddr, srcaddr);
 	}
-	INLINE static PixelCopy& getInstance(SDL_PixelFormat* sfmt, SDL_PixelFormat* dfmt) {
+	static PixelCopy& getInstance(SDL_PixelFormat* sfmt, SDL_PixelFormat* dfmt) {
 		static PixelCopy_Class copier;
 		copier.setformats(sfmt, dfmt);
 		return copier;
@@ -441,68 +441,6 @@ INLINE PixelGet& getPixelGetFunc(const SDL_Surface *surf)  {
 
 /////////////////////
 // Returns a pixel copy functor for the given surface
-INLINE PixelCopy& getPixelCopyFunc(const SDL_Surface *source_surf, const SDL_Surface *dest_surf)
-{
-	const bool issameformat = PixelFormatEqual(source_surf->format, dest_surf->format);
-	const bool alphablend = source_surf->flags & SDL_SRCALPHA;
-	const bool colorkeycheck = source_surf->flags & SDL_SRCCOLORKEY;
-	const bool srchasalpha = source_surf->format->Amask != 0;
-	const bool dsthasalpha = dest_surf->format->Amask != 0;
-	const int srcbytespp = source_surf->format->BytesPerPixel;
-	const int dstbytespp = dest_surf->format->BytesPerPixel;
-
-#define _RET_PIXELCOPY(av1, av2, av3, av4, av5, av6, av7) \
-	return PixelCopy_Class<av1,av2,av3,av4,av5,av6,av7>::getInstance(source_surf->format, dest_surf->format);
-
-#define _BRANCH7(av1, av2, av3, av4, av5, av6) \
-	{ \
-		if(dstbytespp == 4) _RET_PIXELCOPY(av1, av2, av3, av4, av5, av6, 4) \
-		else if(dstbytespp == 3) _RET_PIXELCOPY(av1, av2, av3, av4, av5, av6, 3) \
-		else if(dstbytespp == 2) _RET_PIXELCOPY(av1, av2, av3, av4, av5, av6, 2) \
-		else if(dstbytespp == 1) _RET_PIXELCOPY(av1, av2, av3, av4, av5, av6, 1) \
-		else assert(false); \
-	}
-
-#define _BRANCH6(av1,av2,av3,av4,av5) \
-	{ \
-		if(srcbytespp == 4) _BRANCH7(av1,av2,av3,av4,av5, 4) \
-		else if(srcbytespp == 3) _BRANCH7(av1,av2,av3,av4,av5, 3) \
-		else if(srcbytespp == 2) _BRANCH7(av1,av2,av3,av4,av5, 2) \
-		else if(srcbytespp == 1) _BRANCH7(av1,av2,av3,av4,av5, 1) \
-		else assert(false); \
-	}
-
-#define _BRANCH5(av1,av2,av3,av4) \
-	{ \
-		if(dsthasalpha) _BRANCH6(av1,av2,av3,av4, true) \
-		else _BRANCH6(av1,av2,av3,av4, false); \
-	}
-
-#define _BRANCH4(av1,av2,av3) \
-	{ \
-		if(srchasalpha) _BRANCH5(av1,av2,av3, true) \
-		else _BRANCH5(av1,av2,av3, false); \
-	}
-	
-#define _BRANCH3(av1,av2) \
-	{ \
-		if(colorkeycheck) _BRANCH4(av1,av2, true) \
-		else _BRANCH4(av1,av2, false); \
-	}
-
-#define _BRANCH2(av1) \
-	{ \
-		if(alphablend) _BRANCH3(av1, true) \
-		else _BRANCH3(av1, false); \
-	}
-	
-#define _BRANCH1() \
-	{ \
-		if(issameformat) _BRANCH2(true) \
-		else _BRANCH2(false); \
-	}
-	
-	_BRANCH1()
-}
+PixelCopy& getPixelCopyFunc(const SDL_Surface *source_surf, const SDL_Surface *dest_surf);
 
 #endif
