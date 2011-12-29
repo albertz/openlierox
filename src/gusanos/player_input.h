77a6f9c448d6ec26ec1ae1b058c65696b948979d
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-12-29 22:41:10 +0100

    cleanup

--- src/gusanos/player_input.h
+++ src/gusanos/player_input.h
@@ -14,23 +14,6 @@ void registerPlayerInput();
 std::string eventStart(size_t index, CWormHumanInputHandler::Actions action, std::list<std::string> const& = std::list<std::string>());
 std::string eventStop(size_t index, CWormHumanInputHandler::Actions action, std::list<std::string> const& = std::list<std::string>());
 
-/*
-std::string leftStart(int index, const std::list<std::string> &args);
-std::string leftStop(int index, const std::list<std::string> &args);
-std::string rightStart(int index, const std::list<std::string> &args);
-std::string rightStop(int index, const std::list<std::string> &args);
-std::string upStart(int index, const std::list<std::string> &args);
-std::string upStop(int index, const std::list<std::string> &args);
-std::string downStart(int index, const std::list<std::string> &args);
-std::string downStop(int index, const std::list<std::string> &args);
-std::string fireStart(int index, const std::list<std::string> &args);
-std::string fireStop(int index, const std::list<std::string> &args);
-std::string jumpStart(int index, const std::list<std::string> &args);
-std::string jumpStop(int index, const std::list<std::string> &args);
-std::string changeStart(int index, const std::list<std::string> &args);
-std::string changeStop(int index, const std::list<std::string> &args);
-*/
-
 std::string say( const std::list<std::string> &args );
 
 #endif // _PLAYER_INPUT_H_
