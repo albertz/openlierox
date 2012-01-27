a0694305a6bfb82f43c7fc54f41dcabe5020cd19
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-01-27 19:41:07 +0100

    small fix

--- optional-includes/breakpad/breakpad_logging.h
+++ optional-includes/breakpad/breakpad_logging.h
@@ -2,9 +2,12 @@
 // see libs/breakpad/src/processor/logging.h
 
 struct NullLogStream {
-	template<typename T> std::ostream& operator<<(const T&) {
-		return std::cout;
-	}	
+	template<typename T> NullLogStream& operator<<(const T&) {
+		// do nothing
+		return *this;
+	}
+	// this is for google_breakpad::LogMessageVoidify and BPLOG_IF
+	operator std::ostream&() { return std::cout; }
 };
 
 #define BPLOG_INFO NullLogStream()
