a183dcd83861447ca8ab1bf0733afd3fe3cd33a1
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-03-19 19:56:52 +0100

    cleanup

--- src/gusanos/glua.h
+++ src/gusanos/glua.h
@@ -42,23 +42,6 @@ struct LuaCallbacks
 
 extern LuaCallbacks luaCallbacks;
 
-/*
-// This is GCC specific, because I can't find a way to do it in standard C++ :/
-#define LUA_NEW(t_, param_) \
-({ \
-	void* space = lua.pushObject(t_::metaTable(), sizeof(t_)); \
-	t_* p = new (space) t_ param_; \
-	p->luaReference = lua.createReference(); \
-	p; \
-})
-
-
-#define LUA_DELETE(t_, p_) { \
-	t_* p = (p_); \
-	p->~t_(); \
-	lua.destroyReference(p->luaReference); \
-}*/
-
 struct LuaObject
 {
 	bool deleted;
