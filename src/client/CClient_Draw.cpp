e751a47d43c7f3c217457275d9cc3d6dc95eb333
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-03-27 02:12:04 +0200

    permanentText draw improvement

--- src/client/CClient_Draw.cpp
+++ src/client/CClient_Draw.cpp
@@ -583,7 +583,6 @@ void CClient::Draw(SDL_Surface * bmpDest)
 	
 	if(permanentText != "") {
 		int y = tInterfaceSettings.LocalChatY;
-		tLX->cFont.Draw(bmpDest, tInterfaceSettings.LocalChatX, y, tLX->clWhite, permanentText); // Shadow white
 		tLX->cFont.Draw(bmpDest, tInterfaceSettings.LocalChatX + 1, y+1, tLX->clBlack, permanentText); // Shadow black
 		tLX->cFont.Draw(bmpDest, tInterfaceSettings.LocalChatX, y, Color(200,200,255), permanentText);
 	}
