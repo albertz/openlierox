472cf220df7ea13e69dfb3f28cf23391958f50e9
Dmitry Marakasov <amdmi3@amdmi3.ru>
2012-12-14 23:32:23 +0400

    Fix build on systems that doesn't use glibc

--- src/common/SystemFunctions.cpp
+++ src/common/SystemFunctions.cpp
@@ -63,8 +63,10 @@
 #if defined(__APPLE__) && defined(MAC_OS_X_VERSION_10_6) && MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_6
 #define HAVE_PTHREAD_NAME
 // glibc 2.12 introduced this
-#elif __GLIBC_PREREQ(2,12)
-#define HAVE_PTHREAD_NAME
+#elif defined(__GLIBC_PREREQ)
+#	if __GLIBC_PREREQ(2,12)
+#		define HAVE_PTHREAD_NAME
+#	endif
 #endif
 #endif
 
