081937c9a6c237aac7933c61098c4ef1e71c24e9
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-07-24 18:04:34 +0200

    extend comment class WeakRef

--- src/util/WeakRef.h
+++ src/util/WeakRef.h
@@ -13,8 +13,13 @@
 
 /*
 This is basically `shared_ptr<T*>`, wrapped with some common functions.
+
 This can be used as a base for a weak-pointer, but you need to do the logic
 of resetting it somewhere else - it's not integraded automatically to `T`.
+
+An example is `BaseObject` which has an attribute `WeakRef<BaseObject> thisRef`.
+When you need a `WeakRef` to it, you would copy it from `thisRef`.
+`BaseObject` will automatically reset it when it gets deleted.
 */
 template<typename T>
 struct WeakRef {
