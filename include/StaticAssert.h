ce41dc0960693a1a05e6e5b46df23bd5f9377ffd
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-08-02 17:15:10 +0000

    static_assert define only for < C++11

--- include/StaticAssert.h
+++ include/StaticAssert.h
@@ -10,11 +10,15 @@
 #ifndef __OLX__STATICASSERT_H__
 #define __OLX__STATICASSERT_H__
 
+#if __cplusplus <= 199711L
+
 template <bool b> class static_assert_failure;
 template <> class static_assert_failure<true>  { char foo; };
 template <int s> class static_assert_test{};
 
 #define static_assert(X, desc)  \
 	typedef static_assert_test< (int)sizeof(static_assert_failure< (bool)(X) >) > static_assert_typedef_##desc;
+
+#endif
 
 #endif
