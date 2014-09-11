5b20a6f621235ace900b1e457ac24da7981e22e4
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-09-11 07:50:57 +0200

    more on SurfaceTexture. wip

--- src/client/SurfaceTexture.cpp
+++ src/client/SurfaceTexture.cpp
@@ -6,6 +6,76 @@
 //	code under LGPL
 //
 
+#include <SDL.h>
+#include <assert.h>
 #include "SurfaceTexture.h"
+#include "Debug.h"
 
+SurfaceTexture::SurfaceTexture(const SmartPointer<SDL_Renderer>& renderer, const SmartPointer<SDL_Surface>& surf)
+: w(0), h(0), maxTextureWidth(0), maxTextureHeight(0), numTexturesHoriz(0), numTexturesVert(0)
+{
+	m_renderer = renderer;
+	m_surface = surf;
+	assert(m_renderer.get());
+	assert(m_surface.get());
+	_init();
+}
+
+void SurfaceTexture::_init() {
+	w = m_surface->w;
+	h = m_surface->h;
+	if(w <= 0 || h <= 0) {
+		errors << "SurfaceTexture: invalid surface size" << endl;
+		return;
+	}
+	
+	{
+		SDL_RendererInfo info;
+		if(SDL_GetRendererInfo(m_renderer.get(), &info) == 0) {
+			maxTextureWidth = info.max_texture_width;
+			maxTextureHeight = info.max_texture_height;
+		} else {
+			errors << "SurfaceTexture: GetRendererInfo error: " << SDL_GetError() << endl;
+			return;
+		}
+	}
+
+	numTexturesHoriz = (w - 1) / maxTextureWidth + 1;
+	numTexturesVert = (h - 1) / maxTextureHeight + 1;
+	assert(numTexturesHoriz >= 1 && numTexturesVert >= 1);
+
+	const int numTextures = numTexturesHoriz * numTexturesVert;
+	assert(numTextures >= 1);
+	m_textures.resize(numTextures);
+	for(int i = 0; i < numTextures; ++i) {
+		const int horizIdx = i % numTexturesHoriz;
+		const int vertIdx = i / numTexturesHoriz;
+		
+		int textureWidth, textureHeight;
+		if(horizIdx < numTexturesHoriz - 1) textureWidth = maxTextureWidth;
+		else textureWidth = w % maxTextureWidth;
+		if(vertIdx < numTexturesVert - 1) textureHeight = maxTextureHeight;
+		else textureHeight = w % maxTextureHeight;
+		
+		m_textures[i] = SDL_CreateTexture
+		(
+			m_renderer.get(),
+			m_surface->format->format,
+			SDL_TEXTUREACCESS_STREAMING,
+			textureWidth, textureHeight
+		);
+	}
+}
+
+SurfaceTexture::~SurfaceTexture() {
+	// all are automatically freed via SmartPointer
+}
+
+void SurfaceTexture::updateArea(const SDL_Rect* rect) {
+	
+}
+
+void SurfaceTexture::render(const SDL_Rect * srcrect, const SDL_Rect * dstrect) {
+	
+}
 
--- src/client/SurfaceTexture.h
+++ src/client/SurfaceTexture.h
@@ -9,7 +9,9 @@
 #ifndef __OpenLieroX__SurfaceTexture__
 #define __OpenLieroX__SurfaceTexture__
 
+#include <vector>
 #include "CodeAttributes.h"
+#include "SmartPointer.h"
 
 struct SDL_Renderer;
 struct SDL_Texture;
@@ -17,19 +19,31 @@ struct SDL_Surface;
 struct SDL_Rect;
 
 /*
-This represents a texture
-(might be represented by several small textures - not implemented yet)
+This represents a big virtual texture, composed of multiple smaller textures,
 which is backed up by a surface.
 So, you can update the surface, and then you need to upload the modified
 area back into the texture by updateArea().
 */
 class SurfaceTexture : DontCopyTag {
-	SDL_Surface* m_surface;
-	SDL_Texture* m_texture;
+	SmartPointer<SDL_Renderer> m_renderer;
+	SmartPointer<SDL_Surface> m_surface;
+	std::vector<SmartPointer<SDL_Texture> > m_textures;
 
+	int w, h;
+	int maxTextureWidth, maxTextureHeight;
+	int numTexturesHoriz, numTexturesVert;
+	
+	void _init();
+	
 public:
+	SurfaceTexture(const SmartPointer<SDL_Renderer>& renderer, const SmartPointer<SDL_Surface>& surf);
+	~SurfaceTexture();
+
+	int width() const { return w; }
+	int height() const { return h; }
+	
 	void updateArea(const SDL_Rect* rect);
-	void render(SDL_Renderer* dest, const SDL_Rect * srcrect, const SDL_Rect * dstrect);
+	void render(const SDL_Rect * srcrect, const SDL_Rect * dstrect);
 };
 
 #endif /* defined(__OpenLieroX__SurfaceTexture__) */
