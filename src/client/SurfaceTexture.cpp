b82e0fd195f0b2c40f3d0cda28cb6a40d49b21ea
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-09-11 09:55:21 +0200

    more on SurfaceTexture. wip

--- src/client/SurfaceTexture.cpp
+++ src/client/SurfaceTexture.cpp
@@ -26,6 +26,7 @@ void SurfaceTexture::_init() {
 	h = m_surface->h;
 	if(w <= 0 || h <= 0) {
 		errors << "SurfaceTexture: invalid surface size" << endl;
+		w = h = 0;
 		return;
 	}
 	
@@ -36,6 +37,7 @@ void SurfaceTexture::_init() {
 			maxTextureHeight = info.max_texture_height;
 		} else {
 			errors << "SurfaceTexture: GetRendererInfo error: " << SDL_GetError() << endl;
+			w = h = 0;
 			return;
 		}
 	}
@@ -66,6 +68,7 @@ void SurfaceTexture::_init() {
 		);
 		if(!texture) {
 			errors << "SurfaceTexture: could not create texture: " << SDL_GetError() << endl;
+			w = h = 0;
 			return;
 		}
 		
@@ -79,6 +82,8 @@ SurfaceTexture::~SurfaceTexture() {
 }
 
 void SurfaceTexture::updateArea(const SDL_Rect* _rect) {
+	if(w <= 0) return; // not correctly initialized
+	
 	SDL_Rect rect;
 	if(_rect) rect = *_rect;
 	else {
@@ -104,12 +109,16 @@ void SurfaceTexture::updateArea(const SDL_Rect* _rect) {
 			if(sub.w <= 0) break;
 			
 			int idx = vertIdx * numTexturesHoriz + horizIdx;
-			SDL_Rect srcRect;
-			srcRect.x = sub.x - horizIdx * maxTextureWidth;
-			srcRect.y = sub.y - vertIdx * maxTextureHeight;
-			srcRect.w = sub.w;
-			srcRect.h = sub.h;
-			SDL_RenderCopy(m_renderer.get(), m_textures[idx].get(), &srcRect, &sub);
+			SDL_Rect texRect;
+			texRect.x = sub.x - horizIdx * maxTextureWidth;
+			texRect.y = sub.y - vertIdx * maxTextureHeight;
+			texRect.w = sub.w;
+			texRect.h = sub.h;
+			uint8_t* pixels =
+				(uint8_t*) m_surface->pixels
+				+ sub.y * m_surface->pitch
+				+ sub.x * m_surface->format->BytesPerPixel;
+			SDL_UpdateTexture(m_textures[idx].get(), &texRect, pixels, m_surface->pitch);
 			
 			horizIdx++;
 			sub.x = horizIdx * maxTextureWidth;
@@ -122,7 +131,65 @@ void SurfaceTexture::updateArea(const SDL_Rect* _rect) {
 	}
 }
 
-void SurfaceTexture::render(const SDL_Rect * srcrect, const SDL_Rect * dstrect) {
+void SurfaceTexture::render(const SDL_Rect* _srcrect, const SDL_Rect* _dstrect) {
+	if(w <= 0) return; // not correctly initialized
+
+	SDL_Rect rect;
+	if(_srcrect) rect = *_srcrect;
+	else {
+		// full area
+		rect.x = rect.y = 0;
+		rect.w = w;
+		rect.h = h;
+	}
+	SDL_Rect dstrect;
+	if(_dstrect) dstrect = *_dstrect;
+	else {
+		// full area
+		dstrect.x = dstrect.y = 0;
+		dstrect.w = w;
+		dstrect.h = h;
+	}
 	
+	float scaleX = float(dstrect.w) / rect.w;
+	float scaleY = float(dstrect.h) / rect.h;
+	
+	int vertIdx = rect.y / maxTextureHeight;
+	SDL_Rect sub;
+	sub.y = rect.y;
+	sub.h = (vertIdx + 1) * maxTextureHeight - rect.y;
+	while(vertIdx < numTexturesVert) {
+		if(sub.y + sub.h > rect.y + rect.h) sub.h = rect.y + rect.h - sub.y;
+		if(sub.h <= 0) break;
+		
+		int horizIdx = rect.x / maxTextureWidth;
+		sub.x = rect.x;
+		sub.w = (horizIdx + 1) * maxTextureWidth - rect.x;
+		while(horizIdx < numTexturesHoriz) {
+			if(sub.x + sub.w > rect.x + rect.w) sub.w = rect.x + rect.w - sub.x;
+			if(sub.w <= 0) break;
+			
+			int idx = vertIdx * numTexturesHoriz + horizIdx;
+			SDL_Rect rendSrcRect;
+			rendSrcRect.x = sub.x - horizIdx * maxTextureWidth;
+			rendSrcRect.y = sub.y - vertIdx * maxTextureHeight;
+			rendSrcRect.w = sub.w;
+			rendSrcRect.h = sub.h;
+			SDL_Rect rendDstRect;
+			rendDstRect.x = dstrect.x + int((rect.x - sub.x) * scaleX);
+			rendDstRect.y = dstrect.y + int((rect.y - sub.y) * scaleY);
+			rendDstRect.w = int(sub.w * scaleX);
+			rendDstRect.h = int(sub.h * scaleY);			
+			SDL_RenderCopy(m_renderer.get(), m_textures[idx].get(), &rendSrcRect, &rendDstRect);
+			
+			horizIdx++;
+			sub.x = horizIdx * maxTextureWidth;
+			sub.w = maxTextureWidth;
+		}
+		
+		vertIdx++;
+		sub.y = vertIdx * maxTextureHeight;
+		sub.h = maxTextureHeight;
+	}
 }
 
